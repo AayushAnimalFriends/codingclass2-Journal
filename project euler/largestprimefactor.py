3144c4c380d115dc87d694c31dba0a9b00bc2b12
AayushAnimalFriends <53666929+AayushAnimalFriends@users.noreply.github.com>
2019-12-08 11:15:01 -0800

    its working but slow

--- project euler/largestprimefactor.py
+++ project euler/largestprimefactor.py
@@ -1,24 +1,42 @@
+#
+# Utility Functions
+#
+def IsPrime(n):
+    isitprime = list()
+    for counter in range (1,n+1):
+        holder2 = factor % counter
+        if (holder2 == 0):
+            isitprime.append(counter)
+    if (len(isitprime) == 2):
+        return True
+    else:
+        return False
+##################################################################################################
+
+#n = 600851475143
+n=73232575
 counter = 1
 factors = list()
 isitfactor = 0
-isitprime = list()
+#isitprime = list()
 prime = list()
-for counter in range (600851475143):
+for counter in range (n):
     if (counter >= 1):
-        if (600851475143 % counter == 0):
-            isitfactor = 600851475143 // counter
+        if (n % counter == 0):
+            isitfactor = n // counter
             factors.append(isitfactor)
-for counter2 in range (len(factors) - 1):
-     for counter3 in range (factors[counter2]):
-         holder = factors[counter2]
-         if (counter3 >= 1):
-             holder2 = holder%counter3
-             if (holder2 == 0):
-                 isitprime.append(holder)
-                 if (len(isitprime) == 2):
-                     prime.append(holder)
-                     theanswer = prime[len(prime) - 1]
-                     print(theanswer)
+
+#factors = 1,2,3,4,5,10,20,25,50,97,100
+
+print("All Factors for", n, "are", factors)
+
+for factor in factors:
+    if (IsPrime(factor)):
+        prime.append(factor)
+print("all Primes: ", prime)
+                
+theanswer = prime[0]
+print(theanswer)
                             
                             
                     
