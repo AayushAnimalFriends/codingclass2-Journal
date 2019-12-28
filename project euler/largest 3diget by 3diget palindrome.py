27148aa07925c927f21001d1d4389783fc98b0a0
AayushAnimalFriends <53666929+AayushAnimalFriends@users.noreply.github.com>
2019-12-28 11:14:43 -0800

    Update largest 3diget by 3diget palindrome.py

--- project euler/largest 3diget by 3diget palindrome.py
+++ project euler/largest 3diget by 3diget palindrome.py
@@ -5,20 +5,23 @@ def isitpalindrome(n):
         x = n % 10
         isitpalindrome2.append(x)
         n = n // 10
-    print("My list is: ", isitpalindrome2)
     for counter in range (len(isitpalindrome2) // 2):
         counter2 = len(isitpalindrome2) - counter - 1
-        print("Indexes counter, counter2 is : ", counter, counter2)
         if (isitpalindrome2[counter] == isitpalindrome2[counter2]):
             check.append(1)
     if (len(check) == len(isitpalindrome2) // 2):
         return True
     else:
         return False
-
-
-n=678
-if(isitpalindrome(234567654323)):
-    print('y')
-else:
-    print('n')
+n=999
+thesearepalindromes = list()
+for counter in range(n):
+    for counter2 in range(n):
+       x = (n - counter) * (n - counter2)
+       if(isitpalindrome(x)):
+          print(x)
+          thesearepalindromes.append(x)
+thesearepalindromes.sort()
+print(thesearepalindromes[len(thesearepalindromes) - 1])        
+          
+    
