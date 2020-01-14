9392b35e0605e9ecac49538cc9829b6e9815ae42
AayushAnimalFriends <53666929+AayushAnimalFriends@users.noreply.github.com>
2020-01-14 15:07:12 -0800

    papa's version with functions has extra 1

--- project euler/1-20 LCM v2.py
+++ project euler/1-20 LCM v2.py
@@ -1,3 +1,24 @@
+def LCM(x,y):
+    xm = 1
+    efl = 1
+    while(efl != 0):
+       if(xm % y == 0):
+           efl = 0
+       else:
+           xm = xm + x
+    return xm
+
+
+xm = 1
+for counter in range(1,20):
+    y = counter
+    xm = LCM(y,xm)
+print("Papas Answer is: ",xm)
+    
+    
+#
+#
+#
 x = 1
 y = 1
 xm = 1
